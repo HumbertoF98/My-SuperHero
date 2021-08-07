7cd0ba12fbc696c36d14bfab4fe0c79718d3178f
Humberto <humberto.filho@benie.com.br>
2021-08-06 21:42:08 -0400

    feat(hooks): Create hooks for api calls

---
+++ src/hooks/useSearchHero.ts
@@ -0,0 +1,46 @@
+import { useCallback, useState } from "react";
+import { toast } from "react-toastify";
+
+import api from "../services";
+import { keys } from "../services/keys";
+import { Data } from "../types/heroTypes";
+
+export default function useSearchHero(heroId: string) {
+  const [result, setResult] = useState<Data[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onLoad = useCallback(async () => {
+    if (!heroId) {
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+
+      const { data: resultFind } = await api.get(
+        `public/characters/${heroId}`,
+        {
+          params: {
+            apikey: keys.apikey,
+            ts: keys.ts,
+            hash: keys.hash,
+          },
+        }
+      );
+      const { results } = resultFind.data;
+
+      setResult(results ?? null);
+    } catch (e) {
+      toast.error("Houve um erro ao consultar este herói :(");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [heroId]);
+
+  return {
+    result,
+    isLoading,
+    onLoad,
+  };
+}
---
+++ src/hooks/useSearchHeros.ts
@@ -0,0 +1,44 @@
+import { useCallback, useState } from "react";
+import { toast } from "react-toastify";
+
+import api from "../services";
+import { keys } from "../services/keys";
+import { Data } from "../types/heroTypes";
+
+export default function useSearchHeros(hero: string) {
+  const [result, setResult] = useState<Data[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onLoad = useCallback(async () => {
+    if (!hero) {
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+
+      const { data: resultFind } = await api.get("public/characters", {
+        params: {
+          apikey: keys.apikey,
+          ts: keys.ts,
+          hash: keys.hash,
+          nameStartsWith: hero,
+        },
+      });
+      const { results } = resultFind.data;
+
+      setResult(results ?? null);
+    } catch (e) {
+      toast.error("Houve um erro ao consultar esses heróis :(");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [hero]);
+
+  return {
+    result,
+    isLoading,
+    onLoad,
+  };
+}
